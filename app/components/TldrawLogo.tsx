2c569d58a5194359c95c504ccd3b355ba2811957
CryptoDen1880 <cryptoden1880@gmail.com>
2023-12-09 20:51:01 +0000

    changed url for website

--- app/components/TldrawLogo.tsx
+++ app/components/TldrawLogo.tsx
@@ -5,7 +5,7 @@ export function TldrawLogo() {
 	return (
 		<a
 			className={`tldrawLogo ${breakpoint < 5 ? 'tldrawLogo__mobile' : ''}`}
-			href="https://1c03-2a02-c7c-9a7b-af00-98b-fd65-d998-c4ea.ngrok-free.app/"
+			href="https://metavision-ai.io/"
 		>
 			<h1><span>meta</span>Vision<span>AI</span></h1>
 		</a>
