f76cf24b41e6b6bb671a4d34564d60fc154cf374
CryptoDen1880 <cryptoden1880@gmail.com>
2023-12-11 20:16:25 +0000

    made logo switch from light to dark

--- app/components/TldrawLogo.tsx
+++ app/components/TldrawLogo.tsx
@@ -1,15 +1,28 @@
+
+import { useState, useEffect, use, useContext } from 'react'
 import { useBreakpoint } from '@tldraw/tldraw'
-import logo from '../brain-text.webp'
+import logoWhite from '../brain-text-white.webp'
+import logoBlack from '../brain-text-black.webp'
 import Image from 'next/image'
+import { useEditor } from '@tldraw/tldraw'
+import { uiContext } from '../page'
 
 export function TldrawLogo() {
 	const breakpoint = useBreakpoint()
+	const { user } = useEditor()
+	const { isDarkMode, setIsDarkMode } = useContext(uiContext)
+
+	useEffect(() => {
+		setIsDarkMode(user.getIsDarkMode())
+	}, [user])
+
+	console.log('isDarkMode', isDarkMode)
 	return (
 		<a
 			className={`tldrawLogo ${breakpoint < 5 ? 'tldrawLogo__mobile' : ''}`}
 			href="https://metavision-ai.io/"
 		>
-			<Image src={logo} style={{ width: '120px', height: 'auto' }} height={'200'} width={'120'} alt="MetaVisionAI" />
+			<Image src={isDarkMode ? logoWhite : logoBlack} style={{ width: '120px', height: 'auto' }} height={'200'} width={'120'} alt="MetaVisionAI" />
 		</a>
 	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { createContext, useState } from 'react'
 import dynamic from 'next/dynamic'
 import '@tldraw/tldraw/tldraw.css'
 import { MakeRealButton } from './components/MakeRealButton'
@@ -10,21 +11,38 @@ const Tldraw = dynamic(async () => (await import('@tldraw/tldraw')).Tldraw, {
 	ssr: false,
 })
 
+export const uiContext = createContext({
+	isDarkMode: false,
+	setIsDarkMode: (isDarkMode: boolean) => {},
+})
+
 const shapeUtils = [ResponseShapeUtil]
 
 function App() {
+	const [isDarkMode, setIsDarkMode] = useState(false)
+
+	function handleEvent(name: any, data: any) {
+		// do something with the event
+		if (name === 'toggle-dark-mode') {
+			setIsDarkMode(!isDarkMode)
+		}
+	}
+
 	return (
-		<div className="editor">
-			<Tldraw
-				persistenceKey="make-real"
-				shareZone={<MakeRealButton />}
-				shapeUtils={shapeUtils}
-			>
-				<TldrawLogo />
-				
-				{/* <RiskyButCoolAPIKeyInput /> */}
-			</Tldraw>
-		</div>
+		<uiContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+			<div className="editor">
+				<Tldraw
+					persistenceKey="make-real"
+					shareZone={<MakeRealButton />}
+					shapeUtils={shapeUtils}
+					onUiEvent={handleEvent}
+				>
+					<TldrawLogo />
+					
+					{/* <RiskyButCoolAPIKeyInput /> */}
+				</Tldraw>
+			</div>
+		</uiContext.Provider>
 	)
 }
 
